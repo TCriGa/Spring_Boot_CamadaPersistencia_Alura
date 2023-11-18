30d1b649cde7a854c093c860d30475b974302c61
tcriga <thay.c.gayv@outlook.com>
2023-11-18 15:27:30 -0400

    config e inicializacao flyway

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -14,12 +14,11 @@ import java.util.stream.Collectors
 
 @Service
 class TopicoService(
-        private val repository: TopicoRepository,
-        private val topicoViewMapper: TopicoViewMapper,
-        private val topicoFormMapper: TopicoFormMapper,
-        private val notFoundMessage: String = "Topico nao encontrado!"
+    private val repository: TopicoRepository,
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper,
+    private val notFoundMessage: String = "Topico nao encontrado!"
 ) {
-
     fun listar(): List<TopicoView> {
         return repository.findAll().stream().map { t ->
             topicoViewMapper.map(t)
@@ -28,7 +27,7 @@ class TopicoService(
 
     fun buscarPorId(id: Long): TopicoView {
         val topico = repository.findById(id)
-                .orElseThrow{NotFoundException(notFoundMessage)}
+            .orElseThrow { NotFoundException(notFoundMessage) }
         return topicoViewMapper.map(topico)
     }
 
@@ -40,7 +39,7 @@ class TopicoService(
 
     fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = repository.findById(form.id)
-                .orElseThrow{NotFoundException(notFoundMessage)}
+            .orElseThrow { NotFoundException(notFoundMessage) }
         topico.titulo = form.titulo
         topico.mensagem = form.mensagem
         return topicoViewMapper.map(topico)
